b4d733fb4766c6e6172a207b87b1b03c86ca3dff
Winston Tang <winstontang@Winstons-MBP.attlocal.net>
2022-09-26 19:59:50 -0700

    feat: completed tasks 3.0-3.1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+
 // Task 1.0
 
 const mainEl = document.querySelector('main');
@@ -14,6 +23,32 @@ mainEl.style.backgroundColor = 'var(--main-bg)'
 
 mainEl.innerHTML = '<h1>SEIR Rocks!</h1>';
 
-// Task 1.3
+// Task 1.3 ADD CLASS
 
 mainEl.classList.add('flex-ctr')
+
+// Task 2.0
+var topMenuEl = document.getElementById('top-menu')
+
+// Task 2.1
+topMenuEl.style.height = '100%'
+
+// Task 2.2
+
+topMenuEl.style.background = 'var(--top-menu-bg)'
+
+// Task 2.3
+
+topMenuEl.classList.add('flex-around')
+
+// Task 3.0
+
+
+// Task 3.1
+for(var i = 0; i < menuLinks.length; i++){
+    
+var element = document.createElement('a')
+element.href = menuLinks[i]['href']
+element.text = menuLinks[i]['text']
+topMenuEl.appendChild(element)
+}
